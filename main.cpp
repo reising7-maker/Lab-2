a043e0c3ad3319f305353ff3add35c75066b0800
Levi Reising <reising7@iccc.cc.ia.us>
2026-09-17 09:59:03 -0500

    changed pi defenition

--- main.cpp
+++ main.cpp
@@ -4,12 +4,11 @@
 
 #include <iostream>
 
-
+// Defining the value of pi for calculations
+const float pi = 3.141592653;
 // Giving the definitions and equation to calculate the surface area of the cylinder
 float calculateCylinderSurfaceArea(float radius, float height)
 {
-	//Giving definition to pi for the calculation
-	float pi = 3.14159f;
 	return 2 * pi * radius * (height + radius);
 }
 
@@ -17,8 +16,6 @@ float calculateCylinderSurfaceArea(float radius, float height)
 // Giving the definitions and equation to calculate the volume of the cylinder
 float calculateCylinderVolume(float radius, float height)
 {
-	//Giving definition to pi for the calculation
-	float pi = 3.14159f;
 	return pi * radius * radius * height;
 }
 
@@ -31,18 +28,21 @@ int main()
 	// The radius of the circular base of the cylinder
 	float radius = 5.0;
 	// The height of the cylinder
-	float height = 10.0;
+	float height = 14.0;
 
 	// Showing the radius and height of the cylinder
 	std::cout << "For a cylinder with the radius " << radius << " and height of " << height << std::endl;
 
+	//Putting space between
+	std::cout << "" << std::endl;
+
 	// Calculating the volume of the cylinder
 	float volume = calculateCylinderVolume(radius, height);
-	std::cout << "Volume of this cylinder is: " << volume << std::endl;
+	std::cout << "Volume of this cylinder: " << volume << std::endl;
 
 	// Calculating the surface area of the cylinder
 	float surfaceArea = calculateCylinderSurfaceArea(radius, height);
-	std::cout << "Surface Area of this cylinder is: " << surfaceArea << std::endl;
+	std::cout << "Surface Area of this cylinder: " << surfaceArea << std::endl;
 	
 	// Shows that the program has ended without error
 	return 0;
